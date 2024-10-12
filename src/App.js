eaf442abda8b27fffb19a2e0417f54e19c460e0d
Sarim Zuberi <46082655+sarimzuberi@users.noreply.github.com>
2024-10-11 18:30:31 -0700

    Added debug logs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Using HashRouter for GitHub Pages
 import SeeWhatICanDo from './components/SeeWhatICanDo'; 
 import Navbar from './components/Navbar'; 
 import Hero from './components/Hero';
@@ -13,39 +13,52 @@ import Divider from './components/Divider';
 
 function App() {
   console.log("App is rendering");
+
+  // Log the current environment and basename
   const basename = process.env.NODE_ENV === "production" ? "/portfolio" : "";
+  console.log("Basename is set to:", basename);
 
   return (
-    <Router basename={basename}> {/* Set the basename here */}
+    <Router basename={basename}>
       <div>
+        {/* Log for checking Routes */}
+        {console.log("Routes are being rendered")}
+
         <Routes>
           {/* Main Portfolio Page */}
           <Route
             path="/"
             element={
               <>
+                {console.log("Rendering Main Portfolio Page")}
                 <Navbar />
                 <div id="home">
+                  {console.log("Rendering Hero component")}
                   <Hero />
                 </div>
                 <Divider />
                 <div id="about">
+                  {console.log("Rendering About component")}
                   <About />
                 </div>
                 <Divider />
                 <div id="skills">
+                  {console.log("Rendering Skills component")}
                   <Skills />
                 </div>
                 <Divider />
                 <div id="projects">
+                  {console.log("Rendering Projects component")}
                   <Projects />
                 </div>
                 <Divider />
                 <div id="cv-certificates">
+                  {console.log("Rendering CV and Certificates component")}
                   <CVAndCertificates />
                 </div>
                 <Divider />
                 <div id="contact">
+                  {console.log("Rendering Contact component")}
                   <Contact />
                 </div>
                 <Divider />
@@ -55,7 +68,15 @@ function App() {
           />
           
           {/* See What I Can Do Page */}
-          <Route path="/see-what-i-can-do-page" element={<SeeWhatICanDo />} />
+          <Route 
+            path="/see-what-i-can-do-page" 
+            element={
+              <>
+                {console.log("Rendering See What I Can Do Page")}
+                <SeeWhatICanDo />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
